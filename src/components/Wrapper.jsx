b38de1019eee5f205f2cc3af00386ffe9ecaf921
BGDSHKA <kirill.ateev59@gmail.com>
2021-04-25 19:39:13 +0300

    add lazy component

---
+++ src/components/Wrapper.jsx
@@ -0,0 +1,15 @@
+import React, { Suspense } from 'react'
+
+const LazyComponent = React.lazy(() => import('./LazyComponent'));
+
+const Wrapper = () => {
+    return (
+        <div>
+            <Suspense fallback={<div>Загрузка...</div>}>
+                <LazyComponent />
+            </Suspense>
+        </div>
+    )
+}
+
+export default Wrapper
